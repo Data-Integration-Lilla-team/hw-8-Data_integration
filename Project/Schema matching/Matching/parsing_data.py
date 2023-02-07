e755320102a83d9228abed231b6367a209bff53b
magicWiss <mat.wissel@stud.uniroma3.it>
2023-02-07 12:54:10 +0100

    corretto pars dei dati di 03-gram

--- Project/Schema matching/Matching/parsing_data.py
+++ Project/Schema matching/Matching/parsing_data.py
@@ -9,12 +9,86 @@ riorganizzazinoe valori delle colonne
 '''
 import os
 import pandas as pd
-
+def format_amount_emp(element):
+    divisore=1000
+    format_int=int(element)/divisore
+    return format_int
+#formattazione amount dollari
+def fomrat_amount(element):
+            prefix_03_gram='doll_'
+            trillion='t'
+            billion='b'
+            million='m'
+            trillino_limit=10**12
+            billio_limit=10**9
+            million_limit=10**6
+            int_elem=int(element)
+            #check trillion
+            if int_elem>=trillino_limit:
+                
+                int_elem=int_elem/trillino_limit
+                string_elem=prefix_03_gram+str(int_elem)+' '+trillion
+                
+            elif int_elem<trillino_limit and int_elem>=billio_limit:
+                int_elem=int_elem/billio_limit
+                string_elem=prefix_03_gram+str(int_elem)+' '+billion
+                
+            
+            else:
+                int_elem=int_elem/million_limit
+                string_elem=prefix_03_gram+str(int_elem)+' '+million
+                
+            return string_elem
 class Parser_data:
 
 
     def __init__(self):
         pass
+
+    
+    
+
+    def parse_market_cap_USD_03_gram(self):
+        market_cap_03_gram='Project\\Dataset\\ClusterParsed\\disfold\\03-gren.csv'
+        column_name_03_gram='market_capitalization_usd'
+        
+        
+        ds=pd.read_csv(market_cap_03_gram)
+        #ds[column_name_03_gram]=ds[column_name_03_gram].apply(fomrat_amount)
+        ds=self.drop_not_needed_cols(ds)
+        ds.to_csv(market_cap_03_gram)
+
+    #standardizzaizione del campo employees
+    def parse_employees_number_03_gram(self):
+        to_parse='Project\\Dataset\\ClusterParsed\\valuetoday\\03-gren.csv'
+        col_employees='employees_number'
+        col_market_cap='market_capitalization_2022'
+        
+        ds=pd.read_csv(to_parse)
+        ds=self.drop_not_needed_cols(ds)
+
+        #employees
+        ds[col_employees]=ds[col_employees].fillna(0)
+        ds[col_employees]=ds[col_employees].apply(format_amount_emp)
+
+        #market cap
+        ds[col_market_cap]=ds[col_market_cap].fillna(0)
+        ds[col_market_cap]=ds[col_market_cap].apply(fomrat_amount)
+        print(ds[col_market_cap].head(10))
+        ds.to_csv(to_parse)
+        
+        
+            
+
+    def parse_specific_values(self):
+        #self.parse_market_cap_USD_03_gram()
+        self.parse_employees_number_03_gram()
+
+       
+        
+
+        
+
 
 
 
@@ -68,8 +142,11 @@ class Parser_data:
         dic_to_replace={'%':'_perc',
                         '#':'rank_',
                         '\$': 'doll_',
-                        '\r\n': ''
-        }
+                        '\r\n': '',
+                        'billion':'b',
+                        'trillion':'t',
+                        'million':'m'
+        } 
         columns=data.columns
         cols_to_lower_case=[]
         import re
