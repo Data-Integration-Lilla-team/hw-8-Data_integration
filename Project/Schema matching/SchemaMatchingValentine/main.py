5caaa110ded36114dab8ea7e9837f96efc51f40f
paolo99-stack <paolo99-stack@users.noreply.github.com>
2023-02-09 17:09:24 +0100

    paolo

--- Project/Schema matching/SchemaMatchingValentine/main.py
+++ Project/Schema matching/SchemaMatchingValentine/main.py
@@ -1,19 +1,65 @@
 
-from correlation_matrix_with_valentine import *
-
+from cluster_group_columns import make_cluster_columns
+from excract_score_valentine import score_valentine_and_matrix_correlation
+from extract_schemas import extract_schema_clusters
+from make_schemas import make_schema_cluster
+import os
+import shutil
 import warnings
+
 warnings.filterwarnings('ignore')
 
 
+def remove_and_make_dir(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+        os.mkdir(path)
+    else:
+        os.mkdir(path)
+
+
+def task1():
+
+    print("\nMake cluster columns\n")
+    remove_and_make_dir(csv_columns_path)
+    make_cluster_columns(datasets_base_path, info_path, filename_synonym, csv_columns_path)
+
+
+def task2():
+    print("\nScore Valentine and matrix correlation\n")
+    remove_and_make_dir(dictionary_path)
+    remove_and_make_dir(plot_path)
+    score_valentine_and_matrix_correlation(csv_columns_path, dictionary_path, plot_path, 1000, 8)
+
+
+def task3():
+    print("\nExtract schemas\n")
+    remove_and_make_dir(dict_synonyms_path)
+    extract_schema_clusters(dictionary_path, dict_synonyms_path, 0.1)
+
+
+def task4():
+    print("\nMake schemas\n")
+    remove_and_make_dir(schema_path)
+    make_schema_cluster(datasets_base_path, dict_final_synonyms_path, schema_path)
+
+
 if __name__ == '__main__':
+
     datasets_base_path = "..\\..\\Dataset\\ClusterParsed\\"
-    info_path = ".\\DatasetSchemaMatch\\"
+    info_path = "..\\DatasetSchemaMatch\\"
     filename_synonym = "dic_pre_val.txt"
-    filename_inverted_index = "inverted_index_col_names.txt"
+    csv_columns_path = ".\\columns\\"
     dictionary_path = ".\\dictionary_score\\"
     plot_path = ".\\plot\\"
-    csv_columns_path = ".\\columns\\"
+    dict_synonyms_path = ".\\synonyms\\"
+    dict_final_synonyms_path = ".\\final_synonyms\\"
+    schema_path = ".\\schema\\"
 
+    #task1()
+    #task2()
+    #task3()
+    task4()
 
 
 
---
+++ Project/Schema matching/SchemaMatchingValentine/make_schemas.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+import json
+
+
+def make_schema_cluster(datasets_base_path, dict_synonyms_path, schema_path):
+
+    for cluster_folder_name in os.listdir(datasets_base_path):
+        cluster_path = os.path.join(datasets_base_path, cluster_folder_name)
+        if len(os.listdir(cluster_path)) > 1:
+
+            print("Cluster:", cluster_folder_name)
+
+            file_synonyms_path = os.path.join(dict_synonyms_path, cluster_folder_name + ".txt")
+            file = open(file_synonyms_path)
+            dict_synonyms = json.load(file)
+            file.close()
+
+            inverted_synonym_index = {}
+            for main_token, tokens in dict_synonyms.items():
+                for token in tokens:
+                    tmp = token.split("-")
+                    inverted_synonym_index[tmp[2]] = main_token
+
+            base_schema = list(inverted_synonym_index.keys())
+
+            df = pd.DataFrame(columns=base_schema)
+            for filename in os.listdir(cluster_path):
+
+                dataset_path = os.path.join(cluster_path, filename)
+                group_name = os.path.splitext(filename)[0]
+
+                column_rename = {}
+                tmp = pd.read_csv(dataset_path)
+                for column in list(tmp.columns):
+                    if column in base_schema:
+                        column_rename[column] = inverted_synonym_index[column]
+
+                df_tmp = pd.read_csv(dataset_path)
+                df_tmp = df_tmp.rename(columns=column_rename)
+                df_tmp = df_tmp[df_tmp.columns.intersection(column_rename.keys())]
+                df = pd.concat([df, df_tmp])
+
+            df.to_csv(schema_path + cluster_folder_name + ".csv", index=False)
