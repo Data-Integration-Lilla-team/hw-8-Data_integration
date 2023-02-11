11e7b762299ee4dde874d4102f9242a11cffe477
piebaroni <pietroba99@gmail.com>
2023-02-11 16:16:40 +0100

    start RL

--- Project/Record linkage/get_dataset_to_label.py
+++ Project/Record linkage/get_dataset_to_label.py
@@ -1,30 +1,49 @@
 import deepmatcher as dm
 import py_entitymatching as em
+import time
 
 class getList:
-    def __init__(self, path):
-        self.A = em.read_csv_metadata(path, key='id')
-        self.B = em.read_csv_metadata(path, key='id')
 
-    def generate_list(self):
-        # Basic information about the tables.
-        print('Number of tuples in A: ' + str(len(self.A)))
-        print('Number of tuples in B: ' + str(len(self.B)))
-        print('Number of tuples in A X B (i.e the cartesian product): ' + str(len(self.A)*len(self.B)))
+    def __init__(self):
+        self.dict = {}
+        self.path_training = r"C:\hw-8-Data_integration\Project\Record linkage\Training set"
+        self.path_predict = r"C:\hw-8-Data_integration\Project\Record linkage\Couples for prediction"
+        self.path_original = r"C:\hw-8-Data_integration\Project\Record linkage\Dataset separati per lettera"
 
-        #BLOCKING
+    def generate_list(self, path):
+        start = time.time()
+        A = em.read_csv_metadata(self.path_original + '\\' + path, key='ID', low_memory = False)
+        B = em.read_csv_metadata(self.path_original + '\\' + path, key='ID', low_memory = False)
+
+        A = A.rename(columns={'ceo;': 'ceo'})
+        B = B.rename(columns={'ceo;': 'ceo'})
+
+        block_f = em.get_features_for_blocking(A, B, validate_inferred_attr_types=False)
+
+        rb = em.RuleBasedBlocker()
+        rb.add_rule(['name_name_lev_sim(ltuple, rtuple) < 0.3'], block_f)
+
+        em.set_key(B, 'ID')
+        em.set_key(A, 'ID')
+
         ob = em.OverlapBlocker()
-        K1 = ob.block_tables(self.A, self.B, 'name', 'name',
-                    l_output_attrs=['SCHEMA MEDIATO'], 
-                    r_output_attrs=['SCHEMA MEDIATO'],
-                    overlap_size=2) #DA SCEGLIERE
+        K1 = ob.block_tables(A, B, 'name', 'name', 
+        l_output_attrs=['name', 'country', 'headquarters', 'address', 'founded', 'employees', 'market_cap', 'revenue', 'profit', 'industry', 'sector', 'categories', 'ceo'],
+        r_output_attrs=['name', 'country', 'headquarters', 'address', 'founded', 'employees', 'market_cap', 'revenue', 'profit', 'industry', 'sector', 'categories', 'ceo'],
+        overlap_size=1)
 
-        print(f'Number of comparisons after one step of blocking: {len(K1)}')
-        value = input('Is this number enough? (Y/N)')
+        K2 = ob.block_candset(K1, 'country', 'country', overlap_size=1, allow_missing = True)
 
-        if(value == 'N' or value == 'n'):
-            #CONTIUNA IL BLOCKING
-            K1 = ob.block_tables(K1, 'ATTRIBUTO', 'ATTRIBUTO', overlap_size = 1) #DA SCEGLIERE
+        K3 = rb.block_candset(K2)
+
+        K3.to_csv(self.path_predict + '\\' + path)
+
+        end = time.time()
+        total = end - start
         
-        S = em.sample_table(K1, 100)
-        S.to_csv(r'PATH\to_label.csv', index = False)
+        self.dict[path] = [total]
+
+        S = em.sample_table(K3, 50)
+
+        S.to_csv(self.path_training + '\\' + path)
+
