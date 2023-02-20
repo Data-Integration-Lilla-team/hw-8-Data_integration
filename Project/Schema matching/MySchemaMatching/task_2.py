125e17aeaa96236296413493d800ff7e77904fd6
paolo-di-simone <72343829+paolo-di-simone@users.noreply.github.com>
2023-02-20 22:09:00 +0100

    paolo

--- Project/Schema matching/MySchemaMatching/task_2.py
+++ Project/Schema matching/MySchemaMatching/task_2.py
@@ -2,7 +2,7 @@ import os
 from itertools import combinations
 import pandas as pd
 import json
-
+from multiprocessing import Pool
 
 def jaccard(set1, set2):
     if len(set1) > 0 or len(set2) > 0:
@@ -12,32 +12,38 @@ def jaccard(set1, set2):
     return 0
 
 
-def extract_schema_paolo(directory, name, final_schema_directory, threshold, format):
+def calculate_jaccard(columns):
+    c1, c2, data, threshold = columns
+    s = jaccard(set(data[c1]), set(data[c2]))
+    if s > threshold:
+        return (c1, c2)
+    return None
+
+
+def extract_schema_paolo(directory, name, final_schema_directory, threshold, format, head):
 
     result_jaccard = {}
-    for filename in os.listdir(directory):
 
+    for filename in os.listdir(directory):
         print(filename)
         file = os.path.join(directory, filename)
         df = pd.read_csv(file, encoding="utf-8")
+        df = df.sample(frac=head)
 
-        combs = list(combinations(list(df.columns), 2))
+        combs = list(combinations(df.columns, 2))
         print("Numbero di confronti: ", len(combs))
-        k = 0
-        for c1, c2 in combs:
 
-            print(str(k), "-", c1, "-", c2)
-            k += 1
+        # Calcolare le colonne che soddisfano la soglia in parallelo
+        with Pool() as p:
+            results = p.map(calculate_jaccard, [(c1, c2, df, threshold) for c1, c2 in combs])
 
-            s = jaccard(set(list(df[c1])), set(list(df[c2])))
-
-            if s > threshold:
-
-                print(c1, c2, s)
-
-                if c1 not in result_jaccard.keys():
+        # Aggiungere le colonne che soddisfano la soglia ai risultati finali
+        for r in results:
+            if r is not None:
+                c1, c2 = r
+                if c1 not in result_jaccard:
                     result_jaccard[c1] = set()
-                if c2 not in result_jaccard.keys():
+                if c2 not in result_jaccard:
                     result_jaccard[c2] = set()
 
                 result_jaccard[c1].add(c2)
@@ -54,7 +60,7 @@ def extract_schema_paolo(directory, name, final_schema_directory, threshold, for
     dict_synonyms = {}
     for tokens in s:
         tokens = list(tokens)
-        tmp = tokens[0].split("-")[2]
+        tmp = tokens[0].split("-")[1]
         if tmp not in dict_synonyms.keys():
             dict_synonyms[tmp] = tokens
         else:
@@ -69,13 +75,13 @@ def extract_schema_paolo(directory, name, final_schema_directory, threshold, for
     print("\n\n")
 
 
-def schema_cluster(directory, final_schema_directory, threshold, format):
+def schema_cluster(directory, final_schema_directory, threshold, format, head):
     for dirname in os.listdir(directory):
         print(dirname)
         cluster = os.path.join(directory, dirname)
-        extract_schema_paolo(cluster, dirname, final_schema_directory, threshold, format)
+        extract_schema_paolo(cluster, dirname, final_schema_directory, threshold, format, head)
 
 
 
-def schema_final(directory, final_schema_directory, threshold, format):
-    extract_schema_paolo(directory, "finalschema", final_schema_directory, threshold, format)
+def schema_final(directory, final_schema_directory, threshold, format, head):
+    extract_schema_paolo(directory, "finalschema", final_schema_directory, threshold, format, head)
